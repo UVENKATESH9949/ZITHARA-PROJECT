43cf2a67ceead87f3df080e8c888bb1a3a8cc5d2
UVENKATESH9949 <venkatesh9949.u@gmail.com>
2025-04-06 23:37:55 +0530

    Update Login.jsx

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import './Login.css';
 
-const apiUrl = process.env.REACT_APP_API_BASE_URL || 'https://zithara-jewellery.onrender.com';
+const apiUrl = (process.env.REACT_APP_API_BASE_URL || 'https://zithara-jewellery.onrender.com').replace(/\/$/, '');
 
 const Login = () => {
   const [email, setEmail] = useState('');
